c96324fb9e2756dd741275b07fc7a1cc8ca280e9
Tommaso34 <167868057+Tommaso34@users.noreply.github.com>
2024-04-24 22:22:27 +1200

    Add files via upload

---
+++ plftp-main - Tommso/assets/js/main.js
@@ -0,0 +1,115 @@
+console.log("main.js uploaded");
+const infoCard = document.getElementById("info-card-layer");
+const sidebar = document.getElementById("sidebar");
+const mapContainer = document.getElementById("map");
+const ARViewer = document.getElementById("arviewer-layer");
+const cardTitle = document.getElementById("card-title");
+const cardDescription = document.getElementById("card-description");
+const story = document.getElementById("story");
+/* EDIT20230531A*/
+const storyWebp = document.getElementById("story-webp");
+console.log(storyWebp);
+/* END EDIT20230531A*/
+const videoElem = document.getElementById('video');
+const errorElem = document.getElementById('error');
+const ARviewbtn = document.getElementById('show-ARViewer');
+
+// PLACEHOLDERS FOR CARD CONTENT - DEVELOPMENT ONLY
+const dummyTitle = "Piazza della Repubblica"
+const dummyDescription = "The first place to go to, in order to immediately immerse yourself into the medieval atmosphere of Foligno, is Piazza della Repubblica. Here there is a monument which reminds us that in this place San Francesco got rid of his goods and put on sale his horse and his possessions to restore the Church of San Damiano, in Assisi. In the square there re the most important buildings of Foligno: the Cathedral, Palazzo Comunale, Palazzo dei Podestà and Palazzo Trinci. Many remember Palazzo Comunale because of the tower collapse caught on live tv during the earthquake of 1997. The building is from 13th century, but it was rebuilt between 1546 and 1642. Palazzo del Podestà hosted popular gatherings by the time of Comuni, together with Palazzo Orsini, which houses a beautiful Museum of Printing, an unmissable historica place. Here, Johannes Numeister, pupil of Gutenberg, printed the first edition of Divine Comedy by Dante Alighieri in 1472. Under a glass next to Palazzo del Podestà you can see the “pozzo dei miracoli”, a holy well built in the Middle Ages."
+
+
+//Declare the MediaStreamConstraints object
+const constraints = {
+    audio: false,
+    video: true
+}
+
+const hideInfoCard = () => {
+    console.log("Hide info card");
+    infoCard.setAttribute("hidden", "hidden");
+    sidebar.removeAttribute("hidden");
+    mapContainer.removeAttribute("hidden");
+};
+
+const showInfoCard = (title, description,spritemap) => {
+    console.log("Show info card");
+    //MODIFY WITH DATA FROM MAPBOX
+    cardTitle.innerText = title;
+    cardDescription.innerText = description;
+    //------
+    infoCard.removeAttribute("hidden");
+    sidebar.setAttribute("hidden", "hidden");
+    mapContainer.setAttribute("hidden", "hidden");
+    ARviewbtn.addEventListener("click",showARViewer(spritemap));
+};
+
+const hideSidebar = () => {
+    console.log("Hide sidebar");
+    sidebar.setAttribute("hidden", "hidden");
+};
+
+const showSidebar = () => {
+    console.log("Show sidebar");
+    sidebar.removeAttribute("hidden");
+};
+
+const back2Index = () => {
+    console.log("Show sidebar");
+    window.location.href = "index.html"
+};
+
+const back2Map = () => {
+    //history.back();
+    location.reload();
+}
+
+const hideARViewer = () => {
+    console.log("Hide AR Viewer");
+    sidebar.removeAttribute("hidden");
+    mapContainer.removeAttribute("hidden");
+    ARViewer.setAttribute("hidden", "hidden");
+    video.setAttribute("hidden", "hidden");
+    closeCamera();
+    // Must also stop the camera
+};
+
+const showARViewer = (spritemap) => {
+    console.log("Show AR Viewer");
+    story.setAttribute("src", `./assets/img/sprites/${spritemap}.png`);
+    /* EDIT20230531A*/
+    storyWebp.setAttribute("srcset", `./assets/img/sprites/${spritemap}.webp`);
+    /* END EDIT20230531A*/
+    //ARViewer.setAttribute("width", "70vw"); /*EDITED: REMOVE */
+    openCamera()
+    
+    ARViewer.removeAttribute("hidden");
+    video.removeAttribute("hidden");
+    infoCard.setAttribute("hidden", "hidden");
+    
+}
+
+
+function openCamera() {
+//Ask the User for the access of the device camera and microphone
+    navigator.mediaDevices.getUserMedia({video: {
+        audio: false,
+        facingMode: 'environment'
+      }},constraints)
+    .then(mediaStream => {
+        /* The received mediaStream contains both the
+        video and audio media data*/
+
+        /*Add the mediaStream directly to the source of the video element using the srcObject attribute*/
+        videoElem.srcObject = mediaStream;
+
+    }).catch(err => {
+        // handling the error if any
+        errorElem.innerHTML = err;
+        errorElem.style.display = "block";
+    });
+
+}
+function closeCamera(){
+    videoElem.srcObject = null;
+}
---
+++ plftp-main - Tommso/assets/js/map-bevagna-it.js
@@ -0,0 +1,229 @@
+const imageDir = "bevagna/";
+const langSuffix = "-it";
+
+
+if (!("remove" in Element.prototype)) {
+  Element.prototype.remove = function () {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+mapboxgl.accessToken =
+"pk.eyJ1IjoiZG9jLWRpdmFnbyIsImEiOiJja2NnbXU0ancwdGx1MnhtMm1pdzV5cWd4In0.NXt0RiFp4HjZ_iy55WADkg";
+/**
+ * Add the map to the page
+ */
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v11",
+  center: [12.608060, 42.932704],
+  zoom: 16,
+  scrollZoom: false,
+});
+
+//compass
+const nav = new mapboxgl.NavigationControl({
+  showCompass: true,
+});
+map.on("load", function (e) {
+  map.addControl(nav, "bottom-right");
+});
+var stores = {
+  type: "FeatureCollection",
+  features: [
+    // silvestro
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [12.607940, 42.932720],
+      }, 
+      properties: {
+        phoneFormatted: "",
+        phone: "",
+        address: "Piazza San Silvestro",
+        city: "bevagna",
+        country: "Italy",
+        crossStreet: "",
+        postalCode: "06031",
+        state: "",
+        description:
+          "palla",
+        /* EDIT20230531A*/
+        spritemap: imageDir + "bevagna-san-silvestro" + langSuffix,
+        /* EDIT20230531A*/
+      },
+    },
+ 
+  ],
+};
+/**
+ * Assign a unique id to each store. You'll use this `id`
+ * later to associate each point on the map with a listing
+ * in the sidebar.
+ */
+stores.features.forEach(function (store, i) {
+  store.properties.id = i;
+});
+
+/**
+ * Wait until the map loads to make changes to the map.
+ */
+map.on("load", function (e) {
+  /**
+   * This is where your '.addLayer()' used to be, instead
+   * add only the source without styling a layer
+   */
+  map.addSource("places", {
+    type: "geojson",
+    data: stores,
+  });
+
+  /**
+   * Add all the things to the page:
+   * - The location listings on the side of the page
+   * - The markers onto the map
+   * 
+   */
+  buildLocationList(stores);
+  addMarkers();
+});
+
+/**
+ * Add a marker to the map for every store listing.
+ **/
+function addMarkers() {
+  /* For each feature in the GeoJSON object above: */
+  stores.features.forEach(function (marker) {
+    /* Create a div element for the marker. */
+    var el = document.createElement("div");
+    /* Assign a unique `id` to the marker. */
+    el.id = "marker-" + marker.properties.id;
+    /* Assign the `marker` class to each marker for styling. */
+    el.className = "marker";
+
+    /**
+     * Create a marker using the div element
+     * defined above and add it to the map.
+     **/
+    new mapboxgl.Marker(el, { offset: [0, -23] })
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+
+    /**
+     * Listen to the element and when it is clicked, do three things:
+     * 1. Fly to the point
+     * 2. Close all other popups and display popup for clicked store
+     * 3. Highlight listing in sidebar (and remove highlight for all other listings)
+     **/
+    el.addEventListener("click", function (e) {
+      /* Fly to the point */
+      flyToStore(marker);
+      /* Close all other popups and display popup for clicked store */
+      makeHighlight(marker);
+      showInfoCard(marker.properties.address,marker.properties.description,marker.properties.spritemap);
+      /* Highlight listing in sidebar */
+      e.stopPropagation();
+      
+    });
+  });
+}
+
+/**
+ * Add a listing for each store to the sidebar.
+ **/
+function buildLocationList(data) {
+  data.features.forEach(function (store, i) {
+    /**
+     * Create a shortcut for `store.properties`,
+     * which will be used several times below.
+     **/
+    var prop = store.properties;
+
+    /* Add a new listing section to the sidebar. */
+    var listings = document.getElementById("listings");
+    var listing = listings.appendChild(document.createElement("div"));
+    /* Assign a unique `id` to the listing. */
+    listing.id = "listing-" + prop.id;
+    /* Assign the `item` class to each listing for styling. */
+    listing.className = "item";
+
+    /* Add the link to the individual listing created above. */
+    var link = listing.appendChild(document.createElement("a"));
+    link.href = "#";
+    link.className = "title";
+    link.id = "link-" + prop.id;
+    link.innerHTML = prop.address;
+
+    /**
+     * Listen to the element and when it is clicked, do four things:
+     * 1. Update the `currentFeature` to the store associated with the clicked link
+     * 2. Fly to the point
+     * 3. Close all other popups and display popup for clicked store
+     * 4. Highlight listing in sidebar (and remove highlight for all other listings)
+     **/
+    link.addEventListener("click", function (e) {
+      for (var i = 0; i < data.features.length; i++) {
+        if (this.id === "link-" + data.features[i].properties.id) {
+          var clickedListing = data.features[i];
+          flyToStore(clickedListing);
+          makeHighlight(clickedListing);
+        }
+      }
+      var activeItem = document.getElementsByClassName("active");
+      if (activeItem[0]) {
+        activeItem[0].classList.remove("active");
+      }
+      this.parentNode.classList.add("active");
+    });
+  });
+}
+
+/**
+ * Use Mapbox GL JS's `flyTo` to move the camera smoothly
+ * a given center point.
+ **/
+function flyToStore(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 17,
+  });
+}
+
+
+function makeHighlight(currentFeature) {
+  var marker = document.getElementById(
+    "marker-" + currentFeature.properties.id
+  );
+  marker.style.filter = "hue-rotate(145deg)";
+}
+
+function checkinbounds(currentFeature) {
+  const bound1 = new mapboxgl.LngLat(
+    currentFeature.geometry.coordinates[0] + 0.001,
+    currentFeature.geometry.coordinates[1] + 0.001
+  );
+  const bound2 = new mapboxgl.LngLat(
+    currentFeature.geometry.coordinates[0] - 0.001,
+    currentFeature.geometry.coordinates[1] - 0.001
+  );
+
+  const bounds = new mapboxgl.LngLatBounds(bound1, bound2);
+
+  // called every time a new user position is determined
+  function checkUserPostion(position) {
+    const { latitude, longitude } = position.coords;
+
+    const isUserInBbx = bounds.contains([longitude, latitude]);
+
+    if (isUserInBbx) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  navigator.geolocation.watchPosition(checkUserPostion);
+}
